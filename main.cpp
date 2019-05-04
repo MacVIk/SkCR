54d9f25e99be5c92c967111fac724973815dd0f3
MacVIk <forinform95@mail.ru>
2019-05-04 13:23:49 +0300

    Does not work

--- main.cpp
+++ main.cpp
@@ -1,27 +1,51 @@
-#include "SystemTaskCreator.h"
 
-//#define LEDS_TASK_HANDLE_INDEX          0
-//#define LEDSDIRECTOR_STACK_SIZE configMINIMAL_STACK_SIZE
-//#define LEDSDIRECTOR_PRIORITY (tU32)2
-//#define TIMEOUT 1000000UL;
+#define HSE_VALUE ((uint32_t)8000000)
+
+#include "core_cm4.h"
+#include "iActiveObject.h"
+#include "ReadEncoders.h"
+#include "LEDStrip.h"
+#include "BatteryChargeAsker.h"
+#include "USBUserInterface.h"
+#include "CollisionAvoidance.h"
+#include "MovementControl.h"
+#include "InitialisationList.h"
+#include "UARTtoRS485.h"
+#include "HyroMotor.h"
 
 cRTOS oRTOS;
-SystemTaskCreator* taskCreator;
 
 int main(void)
 {
 	SystemInit();
-//	InitUser.GPIOPinInit();
-//	InitUser.PWMInit();
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+
+	setLEDTask = new LEDStrip();
+	getBatChargeTask = new BatteryChargeAsker();
+	sensorTask = new CollisionAvoidance();
+	usbUserInterface = new USBUserInterface();
+//	moveTask = new MovementControl();
+//	readEncoders = new ReadEncoders();
+	hyroMotor = new HyroMotor();
+
+	InitUser.GPIOPinInit();
+	InitUser.PWMInit();
 //	InitUser.EncoderInit();
 //	InitUser.OnePulseModeInit();
-//	InitUser.ADCInit();
-//	InitUser.InterruptInit();
-//	InitUser.TIMInit();
+	InitUser.ADCInit();
+	InitUser.InterruptInit();
+	InitUser.TIMInit();
 
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
-	taskCreator = SystemTaskCreator::getInstance();
-	taskCreator->taskCreate(1000, 2, "creator");
+	setLEDTask->taskCreate(256, 2, "setLED");
+	getBatChargeTask->taskCreate(256, 2, "getBatCharge");
+	sensorTask->taskCreate(256, 3, "sensorTask");
+	hyroMotor->taskCreate(1025, 4, "hyroMotorTask");
+	usbUserInterface->taskCreate(1024, 4, "UserUARTtoUSB");
+//	moveTask->taskCreate(512,3,"moveTask");
+//	readEncoders->taskCreate(512,3,"readEncoders");
+//	setMotTask->taskCreate(600, 3, "setMotTask");
+
 	oRTOS.startScheduler();
+
 	return 0;
 }
